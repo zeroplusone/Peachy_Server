524470c7b14099e8b4c30dd5a3721fd061ea73a2
zeroplusone <a711186@gmail.com>
2018-07-19 20:59:20 -0700

    change to my server

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+    /*
+    const path = require('path')
+    const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    express()
+      .use(express.static(path.join(__dirname, 'public')))
+      .set('views', path.join(__dirname, 'views'))
+      .set('view engine', 'ejs')
+      .get('/', (req, res) => res.render('pages/index'))
+      .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    */
+
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function(req, res) {
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
+        console.log(data);
+        res.end(data);
+    });
+})
+
+var server = app.listen(8081, function() {
+
+    var host = server.address().address
+    var port = server.address().port
+})
